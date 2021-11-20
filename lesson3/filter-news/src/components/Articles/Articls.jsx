6b3c70d2c06b897664148279186df4b9ac837741
Sergey Savchuk <sergey.savchuk91@gmail.com>
2021-11-20 17:47:13 +0200

    change news filter

--- lesson3/filter-news/src/components/Articles/Articls.jsx
+++ lesson3/filter-news/src/components/Articles/Articls.jsx
@@ -9,40 +9,56 @@ class Articls extends Component {
     newsList: dataList
   }
 
-  filterNews = (filters) => {
-
-    console.log(filters)
-
-    if (filters.special) {
-      this.setState({
-        ...this.state,
-        newsList: [...this.state.newsList].filter(el => el.isSpecial)
-      })
-    } else if (filters.link) {
-      this.setState({
-        ...this.state,
-        newsList: [...this.state.newsList].filter(el => el.link)
-      })
-    } else if (filters.pic) {
-      this.setState({
-        ...this.state,
-        newsList: [...this.state.newsList].filter(el => el.photo)
-      })
-    } else {
-      this.setState({
-        ...this.state,
-        newsList: dataList
-      })
-    }
-
+  setFilters = ({target}) => {
+    this.setState((prev) => ({
+      ...prev,
+      filters: {
+        ...prev.filters,
+        [target.value]: target.checked
+      }
+    }))
   }
 
   render() {
-    const listContent = this.state.newsList.map((article, index) => <ArticleItem key={article.id} article={{...article, index}}/>)
+    const filteredNews = this.state.newsList.filter(el => {
+      const {filters} = this.state
+
+      if(filters?.special && !filters?.pic && !filters?.link) {
+        return el.isSpecial
+      }
+
+      if(!filters?.special && filters?.pic && !filters?.link) {
+        return el.photo
+      }
+
+      if(!filters?.special && !filters?.pic && filters?.link) {
+        return el.link
+      }
+
+      if(filters?.special && filters?.pic && !filters?.link) {
+        return el.isSpecial && el.photo
+      }
+
+      if(!filters?.special && filters?.pic && filters?.link) {
+        return el.link && el.photo
+      }
+
+      if(filters?.special && !filters?.pic && filters?.link) {
+        return el.link && el.isSpecial
+      }
+
+      if(filters?.special && filters?.pic && filters?.link) {
+        return el.link && el.isSpecial && el.photo
+      }
+
+      return true
+    })
+
+    const listContent = filteredNews.map((article, index) => <ArticleItem key={article.id} article={{...article, index}}/>)
 
     return (
         <>
-          <Filters onFilter={this.filterNews}/>
+          <Filters onChange={this.setFilters}/>
           <ul className={classes.list}>
             {listContent}
           </ul>
--- lesson3/filter-news/src/components/Articles/Filters/Filters.jsx
+++ lesson3/filter-news/src/components/Articles/Filters/Filters.jsx
@@ -2,35 +2,23 @@ import React, {Component} from 'react';
 import classes from "./Filters.module.scss"
 
 class Filters extends Component {
-  state = {}
-
-  changeCheckboxHandler = ({target}) => {
-    this.setState({
-      ...this.state,
-      [target.value]: target.checked
-    })
-  }
-
-  filterHandler = () => {
-    this.props.onFilter(this.state)
-  }
 
   render() {
     return (
         <div className={classes["filters-wrapper"]}>
           <div>
             <label htmlFor="special">По "special"</label>
-            <input onChange={this.changeCheckboxHandler} type="checkbox" id="special" value="special"/>
+            <input onChange={this.props.onChange} type="checkbox" id="special" value="special"/>
           </div>
           <div>
             <label htmlFor="pic">По картинке</label>
-            <input onChange={this.changeCheckboxHandler} type="checkbox" id="pic" value="pic"/>
+            <input onChange={this.props.onChange} type="checkbox" id="pic" value="pic"/>
           </div>
           <div>
             <label htmlFor="link">По ссылке</label>
-            <input onChange={this.changeCheckboxHandler} type="checkbox" id="link" value="link"/>
+            <input onChange={this.props.onChange} type="checkbox" id="link" value="link"/>
           </div>
-          <button onClick={this.filterHandler} type="button">Фильтровать</button>
+          <button type="button">Фильтровать</button>
         </div>
     );
   }
